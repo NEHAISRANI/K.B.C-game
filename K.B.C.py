49d8f8a2af93dc27d29e0df2bde033f10e6b0a4c
NEHAISRANI <nehai18@navgurukul.org>
2019-06-16 00:00:04 +0530

    first commit

---
+++ K.B.C.py
@@ -0,0 +1,16 @@
+question_list = ["(1)How many continents are there?",
+                "(2)What is the capital of India",
+                "(3)NG mei kaun se course padhaya jaata hai?"]	
+
+options=[["(1)Four","(2)Nine","(3)Seven", "(4)Eight"],	["(1)Chandigarh","(2)Bhopal", "(3)Chennai", "(4)Delhi"],
+["(1)Software Engineering", "(2)Counseling", "(3)Tourism", "(4)Agriculture"]]
+
+solution_list = [3, 4, 1]
+i=0
+while i<len(options):
+    print question_list[i]
+    j=0
+    while j<len(options[i]):
+        print options[i][j]
+        j=j+1
+    i=i+1
